ec98a1bf4f726ef67cb04c814e6610a1559213b9
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-22 18:53:28 +0900

    mui style edit

--- client/src/pages/api/getPopularMoives.js
+++ client/src/pages/api/getPopularMoives.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   try {
     const apiResponse = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`); //ブラウザで確認できないようにするためサーバー呼び出し
     res.status(200).json(apiResponse.data);
-    console.log("取得結果", apiResponse.data); // frontに返す
+    // console.log("取得結果", apiResponse.data); // frontに返す
   } catch (error) {
     console.log(error);
     res.status(500).json('エラーが発生');
