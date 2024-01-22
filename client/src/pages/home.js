fac769923ec90efbd9780ada0ebd0c3b896c2a26
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-22 12:43:37 +0900

    clean

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -20,24 +20,24 @@ const Home = () => {
     
     return (
         <AppLayout
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    home
-                </h2>
-            }>
-            <Head>
-                <title>Laravel - home</title>
-            </Head>
+					header={
+						<h2 className="font-semibold text-xl text-gray-800 leading-tight">
+								home
+						</h2>
+					}>
+					<Head>
+							<title>Laravel - home</title>
+					</Head>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
+					<div className="py-12">
+						<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+							<div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+								<div className="p-6 bg-white border-b border-gray-200">
+									You're logged in!
+								</div>
+							</div>
+						</div>
+					</div>
         </AppLayout>
     )
 }
