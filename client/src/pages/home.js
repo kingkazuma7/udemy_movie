570921378117a7aba5f52634423a2cfb8e0a42de
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-22 12:50:50 +0900

    途中

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,16 +1,17 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import axios from 'axios';
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
+		const [movies, setMovies] = useState([]);
 
     useEffect(() => {
 		const fetchMovies = async() => {
-			console.log("fetchMovies start");
+			console.log("=== fetchMovies start ===");
 			try {
 				const response = await axios.get('api/getPopularMoives'); // apiエンドポイント呼び出し
-				console.log(response);
+				console.log(response.data.results);
 			} catch (error) {
 				console.log(error);
 			}
@@ -19,26 +20,26 @@ const Home = () => {
     }, []);
     
     return (
-        <AppLayout
-					header={
-						<h2 className="font-semibold text-xl text-gray-800 leading-tight">
-								home
-						</h2>
-					}>
-					<Head>
-							<title>Laravel - home</title>
-					</Head>
+			<AppLayout
+				header={
+					<h2 className="font-semibold text-xl text-gray-800 leading-tight">
+						home
+					</h2>
+				}>
+				<Head>
+						<title>Laravel - home</title>
+				</Head>
 
-					<div className="py-12">
-						<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-							<div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-								<div className="p-6 bg-white border-b border-gray-200">
-									You're logged in!
-								</div>
+				<div className="py-12">
+					<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+						<div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+							<div className="p-6 bg-white border-b border-gray-200">
+								You're logged in!
 							</div>
 						</div>
 					</div>
-        </AppLayout>
+				</div>
+			</AppLayout>
     )
 }
 
